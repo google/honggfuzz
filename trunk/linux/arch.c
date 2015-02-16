6f87edf06126ee97428725e8da8534eef056e96c
robert.swiecki@gmail.com <robert.swiecki@gmail.com@f4334a76-505f-ddcd-06be-78435f765c3c>
2015-02-16 19:06:45 +0000

    Move bfd_init() to each thread

--- trunk/linux/arch.c
+++ trunk/linux/arch.c
@@ -44,7 +44,6 @@
 #include <time.h>
 #include <unistd.h>
 
-#include "linux/bfd.h"
 #include "linux/ptrace.h"
 #include "log.h"
 #include "util.h"
@@ -196,6 +195,5 @@ void arch_reapChild(honggfuzz_t * hfuzz, fuzzer_t * fuzzer)
 
 bool arch_prepareParent(honggfuzz_t * hfuzz)
 {
-    arch_bfdPrepare();
     return arch_ptracePrepare(hfuzz);
 }
