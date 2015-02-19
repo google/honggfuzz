3690e9b4d8888cf1be1aaef66ca90420442073f2
robert.swiecki@gmail.com <robert.swiecki@gmail.com@f4334a76-505f-ddcd-06be-78435f765c3c>
2015-02-19 02:28:25 +0000

    Tmp - O_TRUNC

--- trunk/fuzz.c
+++ trunk/fuzz.c
@@ -373,7 +373,7 @@ static void *fuzz_threadNew(void *arg)
             hfuzz->dynamicFileBestSz = fuzzer.dynamicFileSz;
             hfuzz->branchBestCnt = fuzzer.branchCnt;
 
-            int fd = open("CURRENT_BEST", O_CREAT | O_WRONLY, 0644);
+            int fd = open("CURRENT_BEST", O_CREAT | O_WRONLY | O_TRUNC, 0644);
             files_writeToFd(fd, fuzzer.dynamicFile, fuzzer.dynamicFileSz);
             close(fd);
 
