1760e345133df20016e009636223971954d2a86d
robert.swiecki@gmail.com <robert.swiecki@gmail.com@f4334a76-505f-ddcd-06be-78435f765c3c>
2015-02-25 20:49:35 +0000

    Debug

--- trunk/mangle.c
+++ trunk/mangle.c
@@ -277,20 +277,14 @@ bool mangle_Resize(honggfuzz_t * hfuzz, uint8_t ** buf, size_t * bufSz, int fd)
         *bufSz = (size_t) newSz;
         return true;
     }
-#if defined(_HF_ARCH_LINUX)
-    if (ftruncate(fd, newSz) == -1) {
-        LOGMSG_P(l_ERROR, "Couldn't ftruncate(fd='%d', size='%zu')", fd, newSz);
-        return false;
-    }
-    void *newBuf = mremap(buf, _HF_PAGE_ALIGN_UP(*bufSz), _HF_PAGE_ALIGN_UP(newSz), MREMAP_MAYMOVE);
-#else                           /* defined(_HF_ARCH_LINUX) */
+
     if (ftruncate(fd, newSz) == -1) {
         LOGMSG_P(l_ERROR, "Couldn't ftruncate(fd='%d', size='%zu')", fd, newSz);
         return false;
     }
     void *newBuf = mmap(NULL, _HF_PAGE_ALIGN_UP(newSz), PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
     munmap(buf, _HF_PAGE_ALIGN_UP(*bufSz));
-#endif                          /* defined(_HF_ARCH_LINUX) */
+    LOGMSG(l_ERROR, "UNMAP: %p %zu", buf,  _HF_PAGE_ALIGN_UP(*bufSz));
 
     if (newBuf == MAP_FAILED) {
         LOGMSG_P(l_ERROR, "mremap() failed");
