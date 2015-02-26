ca0c9cd750a787cdb4d3dedf0e804e36380459a3
robert.swiecki@gmail.com <robert.swiecki@gmail.com@f4334a76-505f-ddcd-06be-78435f765c3c>
2015-02-26 00:17:42 +0000

    Logging messages

--- mangle.c
+++ mangle.c
@@ -284,7 +284,6 @@ bool mangle_Resize(honggfuzz_t * hfuzz, uint8_t ** buf, size_t * bufSz, int fd)
         return false;
     }
     void *newBuf = mmap(NULL, _HF_PAGE_ALIGN_UP(newSz), PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
-    LOGMSG(l_ERROR, "UNMAP: %p %zu", buf, _HF_PAGE_ALIGN_UP(*bufSz));
 
     if (newBuf == MAP_FAILED) {
         LOGMSG_P(l_ERROR, "mremap() failed");
