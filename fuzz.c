724f79ebb377008cea364e5b8f4673f841b9b622
robert.swiecki@gmail.com <robert.swiecki@gmail.com@f4334a76-505f-ddcd-06be-78435f765c3c>
2015-02-26 01:47:23 +0000

    Logging

--- fuzz.c
+++ fuzz.c
@@ -338,8 +338,8 @@ static void *fuzz_threadNew(void *arg)
         while (pthread_mutex_lock(&hfuzz->dynamicFile_mutex)) ;
         if (fuzzer.branchCnt >= hfuzz->branchBestCnt) {
             LOGMSG(l_INFO,
-                   "Size: New/Old: %zu/%zu', Branch events Curr/High: %" PRId64 "/%" PRId64,
-                   fuzzer.dynamicFileSz, hfuzz->dynamicFileBestSz, fuzzer.branchCnt,
+                   "New BEST feedback: New/Old: %zu/%zu', Branch events Curr/High: %" PRId64 "/%"
+                   PRId64, fuzzer.dynamicFileSz, hfuzz->dynamicFileBestSz, fuzzer.branchCnt,
                    hfuzz->branchBestCnt);
             memcpy(hfuzz->dynamicFileBest, fuzzer.dynamicFile, fuzzer.dynamicFileSz);
             hfuzz->dynamicFileBestSz = fuzzer.dynamicFileSz;
