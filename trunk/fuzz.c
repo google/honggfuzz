8e397455c385f0cc97eebbcc1cb59d8e5120e674
robert.swiecki@gmail.com <robert.swiecki@gmail.com@f4334a76-505f-ddcd-06be-78435f765c3c>
2015-02-26 14:21:19 +0000

    Improve perf watermarking

--- trunk/fuzz.c
+++ trunk/fuzz.c
@@ -336,8 +336,8 @@ static void *fuzz_threadNew(void *arg)
         while (pthread_mutex_lock(&hfuzz->dynamicFile_mutex)) ;
         if (fuzzer.branchCnt >= hfuzz->branchBestCnt) {
             LOGMSG(l_INFO,
-                   "New BEST feedback: New/Old: %zu/%zu', Perf feedback counter Curr/High: %" PRId64 "/%"
-                   PRId64, fuzzer.dynamicFileSz, hfuzz->dynamicFileBestSz, fuzzer.branchCnt,
+                   "New BEST feedback: New/Old: %zu/%zu', Perf feedback counter Curr/High: %" PRId64
+                   "/%" PRId64, fuzzer.dynamicFileSz, hfuzz->dynamicFileBestSz, fuzzer.branchCnt,
                    hfuzz->branchBestCnt);
             memcpy(hfuzz->dynamicFileBest, fuzzer.dynamicFile, fuzzer.dynamicFileSz);
             hfuzz->dynamicFileBestSz = fuzzer.dynamicFileSz;
