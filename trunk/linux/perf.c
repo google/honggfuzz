9106d953322b9649a7f29f686a88015c2cdafd3a
robert.swiecki@gmail.com <robert.swiecki@gmail.com@f4334a76-505f-ddcd-06be-78435f765c3c>
2015-03-05 20:41:41 +0000

    Add branch over the kernel limit - debugging

--- trunk/linux/perf.c
+++ trunk/linux/perf.c
@@ -68,13 +68,13 @@ static size_t arch_perfCountBranches(void)
 
 static inline void arch_perfAddBranch(uint64_t from, uint64_t to)
 {
-    LOGMSG(l_DEBUG, "Adding branch %#018" PRIx64 " - %#018" PRIx64, from, to);
     /*
      * Kernel sometimes reports branches from the kernel (iret), we are not interested in that as it
      * makes the whole concept of unique branch counting less predictable
      */
     if (__builtin_expect(from > 0xFFFFFFFF00000000, false)
         || __builtin_expect(to > 0xFFFFFFFF00000000, false)) {
+        LOGMSG(l_DEBUG, "Adding branch %#018" PRIx64 " - %#018" PRIx64, from, to);
         return;
     }
     if (from >= perfCutOffAddr || to >= perfCutOffAddr) {
