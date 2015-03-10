b11277f0e7599e93eb11c51235804e0e2466774a
robert.swiecki@gmail.com <robert.swiecki@gmail.com@f4334a76-505f-ddcd-06be-78435f765c3c>
2015-03-10 19:42:30 +0000

    Additional functions in the interceptor

--- interceptor/stringmem.c
+++ interceptor/stringmem.c
@@ -41,6 +41,26 @@ int strncasecmp(const char *s1, const char *s2, size_t n)
     return 0;
 }
 
+char *strstr(const char *haystack, const char *needle)
+{
+    for (size_t i = 0; haystack[i]; i++) {
+        if (strcmp(&haystack[i], needle) == 0) {
+            return (char *)(&haystack[i]);
+        }
+    }
+    return NULL;
+}
+
+char *strcasestr(const char *haystack, const char *needle)
+{
+    for (size_t i = 0; haystack[i]; i++) {
+        if (strcasecmp(&haystack[i], needle) == 0) {
+            return (char *)(&haystack[i]);
+        }
+    }
+    return NULL;
+}
+
 int memcmp(const void *m1, const void *m2, size_t n)
 {
     const char *s1 = (const char *)m1;
@@ -65,4 +85,19 @@ int bcmp(const void *m1, const void *m2, size_t n)
         }
     }
     return 0;
+}
+
+void *memmem(const void *haystack, size_t haystacklen, const void *needle, size_t needlelen)
+{
+    if (needlelen > haystacklen) {
+        return NULL;
+    }
+
+    const char *h = haystack;
+    for (size_t i = 0; i <= (haystacklen - needlelen); i++) {
+        if (memcmp(&h[i], needle, needlelen) == 0) {
+            return (void *)(&h[i]);
+        }
+    }
+    return NULL;
 }
