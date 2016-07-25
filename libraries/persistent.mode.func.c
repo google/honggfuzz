bbfdff62ddd7ef9b91c030f80382e8c74b77cac1
Jagger <robert@swiecki.net>
2016-07-26 01:17:06 +0200

    Split persistent mode into 2 files

---
+++ libraries/persistent.mode.func.c
@@ -0,0 +1,95 @@
+#include <errno.h>
+#include <fcntl.h>
+#include <limits.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#define HF_FUZZ_FD 1023
+#define HF_BUF_SIZE (1024 * 1024 * 16)
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+    static inline ssize_t readFromFd(int fd, uint8_t * buf, size_t len) {
+        size_t readSz = 0;
+        while (readSz < len) {
+            ssize_t sz = read(fd, &buf[readSz], len - readSz);
+            if (sz < 0 && errno == EINTR)
+                continue;
+
+            if (sz == 0)
+                break;
+
+            if (sz < 0)
+                 return -1;
+
+             readSz += sz;
+        } return (ssize_t) readSz;
+    }
+
+    static inline bool readFromFdAll(int fd, uint8_t * buf, size_t len) {
+        return (readFromFd(fd, buf, len) == (ssize_t) len);
+    }
+
+    static bool writeToFd(int fd, uint8_t * buf, size_t len) {
+        size_t writtenSz = 0;
+        while (writtenSz < len) {
+            ssize_t sz = write(fd, &buf[writtenSz], len - writtenSz);
+            if (sz < 0 && errno == EINTR)
+                continue;
+
+            if (sz < 0)
+                return false;
+
+            writtenSz += sz;
+        }
+        return (writtenSz == len);
+    }
+
+    uint8_t buf[HF_BUF_SIZE];
+
+    void HF_ITER(uint8_t ** buf_ptr, size_t * len_ptr) {
+        /*
+         * Send the 'done' marker to the parent
+         */
+        static bool initialized = false;
+
+        if (initialized == true) {
+            uint8_t z = 'A';
+            if (writeToFd(HF_FUZZ_FD, &z, sizeof(z)) == false) {
+                fprintf(stderr, "readFromFdAll() failed");
+                _exit(1);
+            }
+        }
+        initialized = true;
+
+        uint32_t rlen;
+        if (readFromFdAll(HF_FUZZ_FD, (uint8_t *) & rlen, sizeof(rlen)) == false) {
+            fprintf(stderr, "readFromFdAll(size) failed");
+            _exit(1);
+        }
+        size_t len = (size_t) rlen;
+        if (len > HF_BUF_SIZE) {
+            fprintf(stderr, "len (%zu) > buf_size (%zu)", len, (size_t) HF_BUF_SIZE);
+            _exit(1);
+        }
+
+        if (readFromFdAll(HF_FUZZ_FD, buf, len) == false) {
+            fprintf(stderr, "readFromFdAll(buf) failed");
+            _exit(1);
+        }
+
+        *buf_ptr = buf;
+        *len_ptr = len;
+    }
+
+#ifdef __cplusplus
+}
+#endif
--- libraries/persistent.mode.main.c
+++ libraries/persistent.mode.main.c
@@ -1,103 +1,32 @@
-#include <errno.h>
-#include <fcntl.h>
-#include <limits.h>
-#include <stdbool.h>
-#include <stdint.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <signal.h>
-#include <sys/stat.h>
-#include <sys/types.h>
-#include <unistd.h>
+#include "persistent.mode.func.c"
 
-#define HF_FUZZ_FD 1023
-#define HF_BUF_SIZE (1024 * 1024 * 16)
+#ifdef __cplusplus
+extern "C" {
+#endif
 
-static inline ssize_t readFromFd(int fd, uint8_t * buf, size_t len)
-{
-    size_t readSz = 0;
-    while (readSz < len) {
-        ssize_t sz = read(fd, &buf[readSz], len - readSz);
-        if (sz < 0 && errno == EINTR)
-            continue;
+    int LLVMFuzzerTestOneInput(uint8_t * buf, size_t len);
+    __attribute__ ((weak))
+    int LLVMFuzzerInitialize(int *argc, char ***argv);
 
-        if (sz == 0)
-            break;
+    int main(int argc, char **argv) {
+        if (LLVMFuzzerInitialize) {
+            LLVMFuzzerInitialize(&argc, &argv);
+        }
 
-        if (sz < 0)
-            return -1;
+        for (;;) {
+            size_t len;
+            uint8_t *buf;
 
-        readSz += sz;
+            HF_ITER(&buf, &len);
+
+            int ret = LLVMFuzzerTestOneInput(buf, len);
+            if (ret != 0) {
+                printf("LLVMFuzzerTestOneInput() returned '%d'", ret);
+                _exit(1);
+            }
+        }
     }
-    return (ssize_t) readSz;
+
+#ifdef __cplusplus
 }
-
-static inline bool readFromFdAll(int fd, uint8_t * buf, size_t len)
-{
-    return (readFromFd(fd, buf, len) == (ssize_t) len);
-}
-
-static bool writeToFd(int fd, uint8_t * buf, size_t len)
-{
-    size_t writtenSz = 0;
-    while (writtenSz < len) {
-        ssize_t sz = write(fd, &buf[writtenSz], len - writtenSz);
-        if (sz < 0 && errno == EINTR)
-            continue;
-
-        if (sz < 0)
-            return false;
-
-        writtenSz += sz;
-    }
-    return (writtenSz == len);
-}
-
-int LLVMFuzzerTestOneInput(uint8_t * buf, size_t len);
-__attribute__ ((weak))
-int LLVMFuzzerInitialize(int *argc, char ***argv);
-
-int main(int argc, char **argv)
-{
-    uint8_t *buf = (uint8_t *) malloc(HF_BUF_SIZE);
-    if (buf == NULL) {
-        perror("malloc");
-        _exit(1);
-    }
-
-    if (LLVMFuzzerInitialize) {
-        LLVMFuzzerInitialize(&argc, &argv);
-    }
-
-    for (;;) {
-        uint32_t rlen;
-        if (readFromFdAll(HF_FUZZ_FD, (uint8_t *) & rlen, sizeof(rlen)) == false) {
-            fprintf(stderr, "readFromFdAll(size) failed");
-            _exit(1);
-        }
-        size_t len = (size_t) rlen;
-        if (len > HF_BUF_SIZE) {
-            fprintf(stderr, "len (%zu) > buf_size (%zu)", len, (size_t) HF_BUF_SIZE);
-            _exit(1);
-        }
-
-        if (readFromFdAll(HF_FUZZ_FD, buf, len) == false) {
-            fprintf(stderr, "readFromFdAll(buf) failed");
-            _exit(1);
-        }
-
-        int ret = LLVMFuzzerTestOneInput(buf, len);
-        if (ret != 0) {
-            printf("LLVMFuzzerTestOneInput() returned '%d'", ret);
-            _exit(1);
-        }
-
-        /*
-         * Send the 'done' marker to the parent */
-        uint8_t z = 'A';
-        if (writeToFd(HF_FUZZ_FD, &z, sizeof(z)) == false) {
-            fprintf(stderr, "readFromFdAll() failed");
-            _exit(1);
-        }
-    }
-}
+#endif
