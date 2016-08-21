b22c49b610d7e16499e2afaf64f22a4aff15a049
Jagger <robert@swiecki.net>
2016-08-21 19:21:49 +0200

    Just use _exit with gcc instrumentation code

--- libraries/gcc.instrument.func.c
+++ libraries/gcc.instrument.func.c
@@ -1,6 +1,7 @@
 #include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/mman.h>
 #include <sys/stat.h>
 #include <sys/syscall.h>
@@ -10,15 +11,8 @@
 #include <stdatomic.h>
 #endif
 
-#ifdef _HF_ARCH_DARWIN
-#include <stdlib.h>
-#define EXITALL(x)  exit(x)
-#else
-#define EXITALL(x)  syscall(__NR_exit_group, x)
-#endif
-
 #define ATOMIC_PRE_INC(x) __atomic_add_fetch(&(x), 1, __ATOMIC_SEQ_CST)
-#define ATOMIC_POST_OR(x, y) __atomic_fetch_or(&(x), y, __ATOMIC_SEQ_CST)
+#define ATOMIC_POST_OR_RELAXED(x, y) __atomic_fetch_or(&(x), y, __ATOMIC_RELAXED)
 
 static uint8_t *bbMap;
 static uint64_t *bbCnt;
@@ -32,12 +26,12 @@ static void mapBB(void)
     struct stat st;
     if (fstat(1022, &st) == -1) {
         perror("stat");
-        EXITALL(1);
+        _exit(1);
     }
     bbSz = st.st_size - (1024 * 1024);
     if ((bbMap = mmap(NULL, st.st_size, PROT_READ | PROT_WRITE, MAP_SHARED, 1022, 0)) == MAP_FAILED) {
         perror("mmap");
-        EXITALL(1);
+        _exit(1);
     }
     bbCnt = (uint64_t *) & bbMap[bbSz];
     bbCnt[mypid] = 0;
@@ -49,13 +43,8 @@ void __cyg_profile_func_enter(void *func, void *caller)
     size_t byteOff = pos / 8;
     uint8_t bitSet = (uint8_t) (1 << (pos % 8));
 
-    register uint8_t prev = __atomic_fetch_or(&bbMap[byteOff], bitSet, __ATOMIC_RELAXED);
+    register uint8_t prev = ATOMIC_POST_OR_RELAXED(bbMap[byteOff], bitSet);
     if (!(prev & bitSet)) {
         ATOMIC_PRE_INC(bbCnt[mypid]);
     }
-}
-
-void __cyg_profile_func_exit(void *func __attribute__ ((unused)), void *caller
-                             __attribute__ ((unused)))
-{
 }
