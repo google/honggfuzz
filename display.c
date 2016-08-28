7259dde2d298aae7587408193b50b7b98bd2e231
Jagger <robert@swiecki.net>
2016-08-28 15:50:37 +0200

    Slight indent in display.c

--- display.c
+++ display.c
@@ -162,7 +162,7 @@ static void display_displayLocked(honggfuzz_t * hfuzz)
                     ATOMIC_GET(hfuzz->linux.hwCnts.customCnt));
     }
     if (hfuzz->dynFileMethod & _HF_DYNFILE_SOFT) {
-        display_put("  - functions seen: "  ESC_BOLD "%" PRIu64 ESC_RESET "\n",
+        display_put("  - functions seen:  " ESC_BOLD "%" PRIu64 ESC_RESET "\n",
                     ATOMIC_GET(hfuzz->linux.hwCnts.softCnt));
     }
 
