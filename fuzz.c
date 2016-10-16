8e1feb6cb7609636c50bb632c56e7ef301728d96
Robert Swiecki <robert@swiecki.net>
2016-10-16 18:37:25 +0200

    log: Don't print function names with INFO log level

--- fuzz.c
+++ fuzz.c
@@ -367,7 +367,7 @@ static void fuzz_perfFeedback(honggfuzz_t * hfuzz, fuzzer_t * fuzzer)
         hfuzz->linux.hwCnts.softCntCmp += softCntCmp;
 
         LOG_I
-            ("New file size: %zu, Feedback: New (instr,branch,soft,perf,custom): %" PRIu64 "/%"
+            ("New: %zu B., (instr,branch,soft,soft-cmp,perf,custom): %" PRIu64 "/%"
              PRIu64 "/%" PRIu64 "/%" PRIu64 "/%" PRIu64 "/%" PRIu64 ", Total: %" PRIu64 "/%" PRIu64
              "/%" PRIu64 "/%" PRIu64 "/%" PRIu64 "/%" PRIu64, fuzzer->dynamicFileSz,
              fuzzer->linux.hwCnts.cpuInstrCnt, fuzzer->linux.hwCnts.cpuBranchCnt, softCntPc,
--- log.c
+++ log.c
@@ -98,7 +98,7 @@ void logLog(enum llevel_t ll, const char *fn, int ln, bool perr, const char *fmt
         {"F", "\033[7;35m", true},
         {"E", "\033[1;31m", true},
         {"W", "\033[0;33m", true},
-        {"I", "\033[1m", true},
+        {"I", "\033[1m", false},
         {"D", "\033[0;4m", true},
         {"HR", "\033[0m", false},
         {"HB", "\033[1m", false},
