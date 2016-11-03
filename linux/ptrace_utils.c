bfbc78b1d1c64a6da925f665b8891a0fee1080cd
Robert Swiecki <swiecki@google.com>
2016-11-03 16:07:37 +0100

    linux: Use sys_sigabbrev with glibc

--- linux/ptrace_utils.c
+++ linux/ptrace_utils.c
@@ -247,9 +247,9 @@ static ssize_t honggfuzz_process_vm_readv(pid_t pid,
 struct {
     const char *descr;
     bool important;
-} const arch_sigs[_NSIG] = {
-    [0 ... (_NSIG - 1)].important = false,
-    [0 ... (_NSIG - 1)].descr = "UNKNOWN",
+} const arch_sigs[_NSIG + 1] = {
+    [0 ... (_NSIG)].important = false,
+    [0 ... (_NSIG)].descr = "UNKNOWN",
 
     [SIGTRAP].important = false,
     [SIGTRAP].descr = "SIGTRAP",
@@ -278,6 +278,27 @@ struct {
 #ifndef SI_FROMUSER
 #define SI_FROMUSER(siptr)      ((siptr)->si_code <= 0)
 #endif                          /* SI_FROMUSER */
+
+extern const char *sys_sigabbrev[];
+
+static __thread char arch_signame[32];
+static const char *arch_sigName(int signo)
+{
+    if (signo < 0 || signo > _NSIG) {
+        snprintf(arch_signame, sizeof(arch_signame), "UNKNOWN-%d", signo);
+        return arch_signame;
+    }
+    if (signo > __SIGRTMIN) {
+        snprintf(arch_signame, sizeof(arch_signame), "SIG%d-RTMIN+%d", signo, signo - __SIGRTMIN);
+        return arch_signame;
+    }
+#ifdef __ANDROID__
+    return arch_sigs[signo].descr;
+#else
+    snprintf(arch_signame, sizeof(arch_signame), "SIG%s", sys_sigabbrev[signo]);
+    return arch_signame;
+#endif                          /* __ANDROID__ */
+}
 
 static inline char *arch_sanCodeToStr(int exitCode)
 {
@@ -564,7 +585,7 @@ arch_ptraceGenerateReport(pid_t pid, fuzzer_t * fuzzer, funcs_t * funcs, size_t 
                    fuzzer->crashFileName);
     util_ssnprintf(fuzzer->report, sizeof(fuzzer->report), "PID: %d\n", pid);
     util_ssnprintf(fuzzer->report, sizeof(fuzzer->report), "SIGNAL: %s (%d)\n",
-                   arch_sigs[si->si_signo].descr, si->si_signo);
+                   arch_sigName(si->si_signo), si->si_signo);
     util_ssnprintf(fuzzer->report, sizeof(fuzzer->report), "FAULT ADDRESS: %p\n",
                    SI_FROMUSER(si) ? NULL : si->si_addr);
     util_ssnprintf(fuzzer->report, sizeof(fuzzer->report), "INSTRUCTION: %s\n", instr);
@@ -665,7 +686,7 @@ static void arch_ptraceSaveData(honggfuzz_t * hfuzz, pid_t pid, fuzzer_t * fuzze
 
     if (!SI_FROMUSER(&si) && pc && si.si_addr < hfuzz->linux.ignoreAddr) {
         LOG_I("'%s' is interesting (%s), but the si.si_addr is %p (below %p), skipping",
-              fuzzer->fileName, arch_sigs[si.si_signo].descr, si.si_addr, hfuzz->linux.ignoreAddr);
+              fuzzer->fileName, arch_sigName(si.si_signo), si.si_addr, hfuzz->linux.ignoreAddr);
         return;
     }
 
@@ -804,14 +825,14 @@ static void arch_ptraceSaveData(honggfuzz_t * hfuzz, pid_t pid, fuzzer_t * fuzze
     } else if (saveUnique) {
         snprintf(fuzzer->crashFileName, sizeof(fuzzer->crashFileName),
                  "%s/%s.PC.%" REG_PM ".STACK.%" PRIx64 ".CODE.%d.ADDR.%p.INSTR.%s.%s",
-                 hfuzz->workDir, arch_sigs[si.si_signo].descr, pc, fuzzer->backtrace,
+                 hfuzz->workDir, arch_sigName(si.si_signo), pc, fuzzer->backtrace,
                  si.si_code, sig_addr, instr, hfuzz->fileExtn);
     } else {
         char localtmstr[PATH_MAX];
         util_getLocalTime("%F.%H:%M:%S", localtmstr, sizeof(localtmstr), time(NULL));
         snprintf(fuzzer->crashFileName, sizeof(fuzzer->crashFileName),
                  "%s/%s.PC.%" REG_PM ".STACK.%" PRIx64 ".CODE.%d.ADDR.%p.INSTR.%s.%s.%d.%s",
-                 hfuzz->workDir, arch_sigs[si.si_signo].descr, pc, fuzzer->backtrace,
+                 hfuzz->workDir, arch_sigName(si.si_signo), pc, fuzzer->backtrace,
                  si.si_code, sig_addr, instr, localtmstr, pid, hfuzz->fileExtn);
     }
 
