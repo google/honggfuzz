e14865cfdc2f695762a28bd03480f47e970d7dc0
riusksk <riusksk@qq.com>
2016-11-18 22:21:46 +0800

    fix bug "array type 'char [255]' is not assignable"

--- common.h
+++ common.h
@@ -289,7 +289,7 @@ typedef struct {
     const char *origFileName;
     char fileName[PATH_MAX];
     char crashFileName[PATH_MAX];
-    char ext[NAME_MAX];
+    char *ext;
     uint64_t pc;
     uint64_t backtrace;
     uint64_t access;
