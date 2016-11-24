0bcad3c854c212d7dce6b5c7048495b7f44931a7
riusksk <riusksk@qq.com>
2016-11-24 17:18:09 +0800

    update display

--- display.c
+++ display.c
@@ -271,17 +271,18 @@ static void display_displayLocked(honggfuzz_t * hfuzz)
         uint64_t hitBB = ATOMIC_GET(hfuzz->sanCovCnts.hitBBCnt);
         uint64_t totalBB = ATOMIC_GET(hfuzz->sanCovCnts.totalBBCnt);
         float covPer = totalBB ? (((float)hitBB * 100) / totalBB) : 0.0;
-        display_put(ESC_YELLOW "       *** hit #bb:     " ESC_RESET ESC_BOLD "%" _HF_MONETARY_MOD PRIu64 ESC_RESET
+        display_put(ESC_YELLOW "       *** hit #bb    : " ESC_RESET ESC_BOLD "%" _HF_MONETARY_MOD PRIu64 ESC_RESET
                     " (coverage " ESC_BOLD "%.2f" ESC_RESET "%%)\n", hitBB, covPer);
-        display_put(ESC_YELLOW "       *** total #dso:  " ESC_RESET ESC_BOLD "%" _HF_MONETARY_MOD PRIu64 ESC_RESET
+        display_put(ESC_YELLOW "       *** total #dso : " ESC_RESET ESC_BOLD "%" _HF_MONETARY_MOD PRIu64 ESC_RESET
                     " (instrumented only)\n", ATOMIC_GET(hfuzz->sanCovCnts.iDsoCnt));
-        display_put(ESC_YELLOW "       *** new #bb:     " ESC_RESET ESC_BOLD "%" _HF_MONETARY_MOD PRIu64 ESC_RESET
-                    " (" ESC_WHITE "last update:" ESC_RESET ESC_BOLD " %s)\n" ESC_RESET, ATOMIC_GET(hfuzz->sanCovCnts.newBBCnt), 
-                        get_time_elapsed(ATOMIC_GET(hfuzz->sanCovCnts.lastBBTime)));          
-        display_put(ESC_YELLOW "       *** crashes:     " ESC_RESET ESC_BOLD "%" _HF_MONETARY_MOD PRIu64 ESC_RESET
+        display_put(ESC_YELLOW "       *** new #bb    : " ESC_RESET ESC_BOLD "%" _HF_MONETARY_MOD PRIu64 ESC_RESET
+                    " (" ESC_WHITE "last update:" ESC_RESET ESC_BOLD " %s)\n" ESC_RESET, 
+                    ATOMIC_GET(hfuzz->sanCovCnts.newBBCnt), 
+                    get_time_elapsed(ATOMIC_GET(hfuzz->sanCovCnts.lastBBTime)));          
+        display_put(ESC_YELLOW "       *** crashes    : " ESC_RESET ESC_BOLD "%" _HF_MONETARY_MOD PRIu64 ESC_RESET
                     "\n", ATOMIC_GET(hfuzz->sanCovCnts.crashesCnt));
     }
-    display_put("--------------------------------------[ " ESC_BOLD "LOGS" ESC_RESET 
+    display_put("--------------------------------------[ " ESC_BOLD ESC_YELLOW "LOGS" ESC_RESET 
                 " ]--------------------------------------\n");
 }
 
