a7ecf162c6a0a27107840a762a24e6d5b219f83f
Robert Swiecki <robert@swiecki.net>
2017-02-12 23:17:41 +0100

    Send SIGTERM instead of SIGKILL when execve fails

--- subproc.c
+++ subproc.c
@@ -249,7 +249,7 @@ static bool subproc_New(honggfuzz_t * hfuzz, fuzzer_t * fuzzer)
         }
         if (!arch_launchChild(hfuzz, fuzzer->fileName)) {
             LOG_E("Error launching child process");
-            kill(hfuzz->mainPid, SIGKILL);
+            kill(hfuzz->mainPid, SIGTERM);
             _exit(EXIT_FAILURE);
         }
         abort();
