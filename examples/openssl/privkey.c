964d1ffc20e1cbd1c7a655df796f2cba189c01bb
Robert Swiecki <robert@swiecki.net>
2017-02-25 05:20:34 +0100

    examples/openssl: privkey.c from boringssl, plus initial corpus

---
+++ examples/openssl/privkey.c
@@ -0,0 +1,16 @@
+#include <openssl/err.h>
+#include <openssl/evp.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int LLVMFuzzerTestOneInput(const uint8_t* buf, size_t len)
+{
+    EVP_PKEY_free(d2i_AutoPrivateKey(NULL, &buf, len));
+    return 0;
+}
+
+#ifdef __cplusplus
+}
+#endif
