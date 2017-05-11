588fc81a4f99913afcadb8d371a7aeaf8fb5fcf7
Robert Swiecki <robert@swiecki.net>
2017-05-11 03:36:03 +0200

    display: CPU -> CPU%

--- display.c
+++ display.c
@@ -191,7 +191,7 @@ static void display_displayLocked(honggfuzz_t * hfuzz)
     }
     unsigned cpuUse = getCpuUse(num_cpu);
     display_put("     Threads : " ESC_BOLD "%zu" ESC_RESET ", CPUs: " ESC_BOLD "%ld" ESC_RESET
-                ", CPU: " ESC_BOLD "%u" ESC_RESET "%% (" ESC_BOLD "%u" ESC_RESET "%%/CPU)\n",
+                ", CPU%: " ESC_BOLD "%u" ESC_RESET "%% (" ESC_BOLD "%u" ESC_RESET "%%/CPU)\n",
                 hfuzz->threadsMax, num_cpu, cpuUse, cpuUse / num_cpu);
 
     display_put("       Speed : " ESC_BOLD "% " _HF_MONETARY_MOD "zu" ESC_RESET "/sec"
