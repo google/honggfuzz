google/honggfuzz

commit d51cd3fb934d991c9867b5c684479402334d0530

libhfuzz: Support old __sanitizer_cov_trace_cmp

libhfuzz/instrument.c
@@ -173,6 +173,22 @@ ATTRIBUTE_X86_REQUIRE_SSE42 void __sanitizer_cov_trace_switch(uint64_t Val, uint
     }
 }
 
+ATTRIBUTE_X86_REQUIRE_SSE42 void __sanitizer_cov_trace_cmp(uint64_t SizeAndType, uint64_t Arg1,
+                                                           uint64_t Arg2)
+{
+    uint64_t CmpSize = (SizeAndType >> 32) / 8;
+    switch (CmpSize) {
+    case (sizeof(uint8_t)):
+        return __sanitizer_cov_trace_cmp1(Arg1, Arg2);
+    case (sizeof(uint16_t)):
+        return __sanitizer_cov_trace_cmp2(Arg1, Arg2);
+    case (sizeof(uint32_t)):
+        return __sanitizer_cov_trace_cmp4(Arg1, Arg2);
+    case (sizeof(uint64_t)):
+        return __sanitizer_cov_trace_cmp8(Arg1, Arg2);
+    }
+}
+
 /*
  * -fsanitize-coverage=trace-pc-guard
  */

commit bae12bb883d00ba1bf6c4ddbfaed5ad8638c3ecb

[macOS clang] fix libhfuzz compilation issue
Fixes a void return expression issue when compiling with clang. Issue was
spotted against macOS and was introduced by commit
https://github.com/google/honggfuzz/commit/d51cd3fb934d991c9867b5c684479402334d0530

libhfuzz/instrument.c:182:9: error: void function '__sanitizer_cov_trace_cmp' should not return void expression [-Werror,-Wpedantic]
        return __sanitizer_cov_trace_cmp1(Arg1, Arg2);
        ^      ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
libhfuzz/instrument.c:184:9: error: void function '__sanitizer_cov_trace_cmp' should not return void expression [-Werror,-Wpedantic]
        return __sanitizer_cov_trace_cmp2(Arg1, Arg2);
        ^      ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
libhfuzz/instrument.c:186:9: error: void function '__sanitizer_cov_trace_cmp' should not return void expression [-Werror,-Wpedantic]
        return __sanitizer_cov_trace_cmp4(Arg1, Arg2);
        ^      ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
libhfuzz/instrument.c:188:9: error: void function '__sanitizer_cov_trace_cmp' should not return void expression [-Werror,-Wpedantic]
        return __sanitizer_cov_trace_cmp8(Arg1, Arg2);
        ^      ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
4 errors generated.
make: *** [libhfuzz/instrument.o] Error 1

Signed-off-by: Anestis Bechtsoudis <anestis@census-labs.com>

libhfuzz/instrument.c
@@ -179,13 +179,13 @@ ATTRIBUTE_X86_REQUIRE_SSE42 void __sanitizer_cov_trace_cmp(uint64_t SizeAndType,
     uint64_t CmpSize = (SizeAndType >> 32) / 8;
     switch (CmpSize) {
     case (sizeof(uint8_t)):
-        return __sanitizer_cov_trace_cmp1(Arg1, Arg2);
+        __sanitizer_cov_trace_cmp1(Arg1, Arg2);
     case (sizeof(uint16_t)):
-        return __sanitizer_cov_trace_cmp2(Arg1, Arg2);
+        __sanitizer_cov_trace_cmp2(Arg1, Arg2);
     case (sizeof(uint32_t)):
-        return __sanitizer_cov_trace_cmp4(Arg1, Arg2);
+        __sanitizer_cov_trace_cmp4(Arg1, Arg2);
     case (sizeof(uint64_t)):
-        return __sanitizer_cov_trace_cmp8(Arg1, Arg2);
+        __sanitizer_cov_trace_cmp8(Arg1, Arg2);
     }
 }
 