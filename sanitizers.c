d6f1c89740890b3295377fe2d2abfef65494bd0e
Robert Swiecki <robert@swiecki.net>
2017-07-27 17:43:39 +0200

    sanitizers: use stderr as pid will be appended to /dev/tty

--- sanitizers.c
+++ sanitizers.c
@@ -96,7 +96,7 @@
 /* If no sanitzer support was requested, simply make it use abort() on errors */
 #define kSAN_REGULAR        "abort_on_error=1:handle_segv=0:handle_sigbus=0:handle_abort=0:" \
                             "handle_sigill=0:handle_sigfpe=0:allocator_may_return_null=1:" \
-                            "symbolize=1:detect_leaks=0:disable_coredump=0:log_path=/dev/tty"
+                            "symbolize=1:detect_leaks=0:disable_coredump=0:log_path=stderr"
 
 /*
  * If the program ends with a signal that ASan does not handle (or can not
