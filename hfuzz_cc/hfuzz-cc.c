d3276bd575e21d31ccc75965d2ab4805d4d465e0
Robert Swiecki <robert@swiecki.net>
2017-11-01 16:46:48 +0100

    hfuzz-cc: no longer need to link with BlocksRuntime

--- hfuzz_cc/hfuzz-cc.c
+++ hfuzz_cc/hfuzz-cc.c
@@ -296,10 +296,6 @@ static int ldMode(int argc, char** argv)
     args[j++] = lhfuzzPath;
     args[j++] = "-Wl,--no-whole-archive";
 
-/* libcommon.a will use it when compiled with clang */
-#if defined(__clang__)
-    args[j++] = "-lBlocksRuntime";
-#endif /*  defined(__clang__) */
     args[j++] = "-lpthread";
 
     return execCC(j, args);
