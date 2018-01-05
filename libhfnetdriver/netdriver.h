5043d9b39a6d31f7985763f89d329ec4f59f0b4a
Robert Swiecki <robert@swiecki.net>
2018-01-05 17:07:39 +0100

    Add libhfnetdriver/netdriver.h

---
+++ libhfnetdriver/netdriver.h
@@ -0,0 +1,14 @@
+#ifndef _HF_LIBHFNETDRIVER_NETDRIVER
+#define _HF_LIBHFNETDRIVER_NETDRIVER
+
+#ifdef __cplusplus
+extern "C" {
+#endif  /* ifdef __cplusplus */
+
+int HonggfuzzNetDriver_main(int argc, char **argv);
+
+#ifdef __cplusplus
+}
+#endif  /* ifdef __cplusplus */
+
+#endif  /* ifndef _HF_LIBHFNETDRIVER_NETDRIVER */
