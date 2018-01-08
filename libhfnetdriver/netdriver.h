e31ba997f9710ca9f6add4f08e7d03282c4e86e3
Robert Swiecki <robert@swiecki.net>
2018-01-08 17:15:45 +0100

    hfnetdriver: sorround include by extern 'C'

--- libhfnetdriver/netdriver.h
+++ libhfnetdriver/netdriver.h
@@ -1,28 +1,9 @@
-/*
- *
- * honggfuzz - network driver for fuzzing
- * -----------------------------------------
- *
- * Author: Robert Swiecki <swiecki@google.com>
- *
- * Copyright 2018 by Google Inc. All Rights Reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may
- * not use this file except in compliance with the License. You may obtain
- * a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
- * implied. See the License for the specific language governing
- * permissions and limitations under the License.
- *
- */
-
 #ifndef _HF_NETDRIVER_NETDRIVER_H
 #define _HF_NETDRIVER_NETDRIVER_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 #include <inttypes.h>
 #include <stdint.h>
@@ -36,5 +17,9 @@ int HonggfuzzNetDriverArgsForServer(int argc, char **argv, int *server_argc, cha
  * TCP port that the fuzzed data inputs will be sent to
  */
 uint16_t HonggfuzzNetDriverPort(int argc, char **argv);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif /* ifndef _HF_NETDRIVER_NETDRIVER_H_ */
