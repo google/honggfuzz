4501a269ecaaa7019eab5828915d6dccbf8c3130
Robert Swiecki <robert@swiecki.net>
2018-01-09 17:56:22 +0100

    netdriver: MAIN_OR_HFND_FUZZ_TARGET -> HFND_FUZZING_ENTRY_FUNCTION

--- libhfnetdriver/netdriver.h
+++ libhfnetdriver/netdriver.h
@@ -9,9 +9,9 @@ extern "C" {
 #endif
 
 #ifdef __cplusplus
-#define MAIN_OR_HFND_FUZZ_TARGET(x, y) extern "C" __attribute__((used)) int HonggfuzzNetDriver_main(x, y)
+#define HFND_FUZZING_ENTRY_FUNCTION(x, y) extern "C" __attribute__((used)) int HonggfuzzNetDriver_main(x, y)
 #else /* ifdef __cplusplus */
-#define MAIN_OR_HFND_FUZZ_TARGET(x, y) __attribute__((used)) int HonggfuzzNetDriver_main(x, y)
+#define HFND_FUZZING_ENTRY_FUNCTION(x, y) __attribute__((used)) int HonggfuzzNetDriver_main(x, y)
 #endif /* ifdef __cplusplus */
 
 /*
