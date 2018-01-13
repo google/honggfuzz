31f166f6462ebb833bda3129338ddf56c226a9df
李景 <riusksk@qq.com>
2018-01-13 16:39:59 +0800

    fix bug

--- tools/bing_pdf_download.sh
+++ tools/bing_pdf_download.sh
@@ -1,2 +1,2 @@
-for ((i=1;i<281000000;i=i+10));do curl -s https://www.bing.com/search\?q\=filetype%3apdf\&first\=$i\&FORM\=PERE |awk -F '"' '{for(i=1;i<NF;i++){if(match($i,/\.pdf$/)){print $i}}}'|xargs -I {} wget {} -P ./bing_download ;done
+j=0;for ((i=1;i<281000000/10;i++));do let j=j+10;echo '===Page '`expr $(($j/10))`'==='; curl -s https://www.bing.com/search\?q\=filetype%3apdf\&first\=$j\&FORM\=PERE |awk -F '"' '{for(i=1;i<NF;i++){if(match($i,/\.pdf$/)){print $i}}}'|xargs -I {} wget {} -P ./bing_download ;done
 for f in ./bing_download/*;do file $f|grep -v PDF|awk -F ':' '{print $1}'|xargs rm;done
