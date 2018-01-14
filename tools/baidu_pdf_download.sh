9bec89e733af27fdfe076277f6b20e6b140d19de
李景 <riusksk@qq.com>
2018-01-14 09:54:46 +0800

    fix bug

--- tools/baidu_pdf_download.sh
+++ tools/baidu_pdf_download.sh
@@ -1,3 +1,2 @@
-for ((i=0;i<10000;i++));do curl -s "http://www.baidu.com/s?wd=filetype%3Apdf&pn="$i"0" |grep -o "{\"title\":.*\"url\":\"http://www.baidu.com/link?url.*\"}'"|awk -F '"' '{print $8}'|xargs -I {} wget {} -P ./baidu_download;done
+for ((i=0;i<10000;i++));do curl -s "http://www.baidu.com/s?wd=filetype%3Apdf&pn="$i"0" |grep -o "{\"title\":.*\"url\":\"http://www.baidu.com/link?url.*\"}'"|awk -F '"' '{print $8}'|xargs -I {} curl -s -I {}|grep -oi 'http.*\.pdf'|xargs -I {} wget -t2 {} -P ./baidu_download;done
 for f in ./baidu_download/*;do file $f|grep -v PDF|awk -F ':' '{print $1}'|xargs rm;done
-j=0;for f in ./baidu_download/*;do let j=j+1;mv ./baidu_download/$f ./baidu_download/$j.pdf;done
