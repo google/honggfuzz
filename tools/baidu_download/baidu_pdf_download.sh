90694e9192c907ff32a2e00c044bd763e33f7886
李景 <riusksk@qq.com>
2018-01-15 21:46:55 +0800

    add keyword index download

---
+++ tools/baidu_download/baidu_pdf_download.sh
@@ -0,0 +1,2 @@
+for ((i=0;i<10000;i++));do curl -s "http://www.baidu.com/s?wd=filetype%3Apdf&pn="$i"0" |grep -o "{\"title\":.*\"url\":\"http://www.baidu.com/link?url.*\"}'"|awk -F '"' '{print $8}'|xargs -I {} curl -s -I {}|grep -oi 'http.*\.pdf'|xargs -I {} wget -t2 {} -P ./download;done
+for f in ./download/*;do file $f|grep -v PDF|awk -F ':' '{print $1}'|xargs rm;done
