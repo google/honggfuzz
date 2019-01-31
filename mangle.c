707ceb451c8b7c6bab410ab54367b27c1a3c6a7b
Robert Swiecki <robert@swiecki.net>
2019-01-31 12:04:58 +0100

    mangle: append NUL-byte to the buffer

--- mangle.c
+++ mangle.c
@@ -947,4 +947,9 @@ void mangle_mangleContent(run_t* run) {
             mangleFuncs[choice](run);
         }
     }
+
+    /* Add NUL-byte to the buffer if smaller than the max size */
+    if (run->dynamicFileSz < run->global->mutate.maxFileSz) {
+        run->dynamicFile[run->dynamicFileSz] = '\0';
+    }
 }
