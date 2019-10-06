88c1b44d053fb2992cadbb4efe7dc3de8e7eb0a1
Robert Swiecki <robert@swiecki.net>
2019-10-06 21:29:36 +0200

    Corpus minimization: sort files prior to testing coverage #2

--- fuzz.c
+++ fuzz.c
@@ -179,8 +179,8 @@ static void fuzz_setDynamicMainState(run_t* run) {
     ATOMIC_SET(run->global->cfg.switchingToFDM, false);
 
     if (run->global->cfg.minimize) {
-        LOG_I("Entering phase 3/3: Corpus Minimization");
         input_sortDynamicInput(run->global);
+        LOG_I("Entering phase 3/3: Corpus Minimization");
         ATOMIC_SET(run->global->feedback.state, _HF_STATE_DYNAMIC_MINIMIZE);
         return;
     }
