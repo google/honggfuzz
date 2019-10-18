47fe879255d3b1caac17f188c68fa6f1d6684b9d
Robert Swiecki <robert@swiecki.net>
2019-10-18 19:51:19 +0200

    mangle: add mangle_Resize explicity

--- mangle.c
+++ mangle.c
@@ -621,6 +621,7 @@ void mangle_mangleContent(run_t* run) {
         mangle_Expand,
         mangle_Shrink,
         mangle_ASCIIVal,
+        mangle_Resize,
     };
 
     if (run->mutationsPerRun == 0U) {
