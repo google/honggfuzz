3d78e4b5555cf2ba533124b5b4ec41bec6659a35
Robert Swiecki <robert@swiecki.net>
2019-10-29 12:33:26 +0100

    examples/openssl: fix compile_hfuzz_openssl_master.sh for openssl-master

--- examples/openssl/compile_hfuzz_openssl_master.sh
+++ examples/openssl/compile_hfuzz_openssl_master.sh
@@ -4,6 +4,6 @@ CC=~/src/honggfuzz/hfuzz_cc/hfuzz-clang CXX="$CC"++ ./config \
   -DPEDANTIC no-shared -DFUZZING_BUILD_MODE_UNSAFE_FOR_PRODUCTION -O0 \
   -fno-sanitize=alignment -lm -ggdb -gdwarf-4 --debug -fno-omit-frame-pointer \
   enable-tls1_3 enable-weak-ssl-ciphers enable-rc5 enable-md2 \
-  enable-ssl3 enable-ssl3-method enable-nextprotoneg enable-heartbeats enable-tls13downgrade \
+  enable-ssl3 enable-ssl3-method enable-nextprotoneg enable-heartbeats \
   enable-aria enable-zlib enable-egd \
   $@
