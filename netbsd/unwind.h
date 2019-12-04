1586d05ebbb7ab57011416369b576cf53bd2db82
Robert Swiecki <robert@swiecki.net>
2019-12-04 23:28:30 +0100

    netbsd: use funcs_t from sanitizers.h

--- netbsd/unwind.h
+++ netbsd/unwind.h
@@ -27,29 +27,10 @@
 #include <sys/param.h>
 #include <sys/types.h>
 
+#include "sanitizers.h"
+
 /* String buffer size for function names in stack traces produced from libunwind */
 #define _HF_FUNC_NAME_SZ 256  // Should be alright for mangled C++ procs too
-
-#define _HF_MAX_FUNCS 80
-typedef struct {
-    void* pc;
-
-    /* If ASan custom parsing, function not available without symbolication */
-    char func[_HF_FUNC_NAME_SZ];
-
-    /*
-     * If libuwind proc maps is used to retrieve map name
-     * If ASan custom parsing it's retrieved from generated report file
-     */
-    char mapName[PATH_MAX];
-
-    /*
-     * If libunwind + bfd symbolizer, line is actual symbol file line
-     * If libunwind + custom (e.g. Android), line is offset from function symbol
-     * If ASan custom parsing, line is offset from matching map load base address
-     */
-    size_t line;
-} funcs_t;
 
 extern char* arch_btContainsSymbol(
     size_t symbolsListSz, char** symbolsList, size_t num_frames, funcs_t* funcs);
