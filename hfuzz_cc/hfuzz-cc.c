fcacdbf39510939ebf316024cafa9ba0c77a0f78
Robert Swiecki <robert@swiecki.net>
2020-03-08 03:41:45 +0100

    libhfuzz/hfuzz-cc: add glib memorycmp wrappers #2

--- hfuzz_cc/hfuzz-cc.c
+++ hfuzz_cc/hfuzz-cc.c
@@ -415,7 +415,8 @@ static int ldMode(int argc, char** argv) {
     args[j++] = "-Wl,--wrap=cmsstrcasecmp";
     /* GLib wrappers */
     args[j++] = "-Wl,--wrap=g_strcmp0";
-    args[j++] = "-Wl,--wrap=g_strcasecmp", args[j++] = "-Wl,--wrap=g_strncasecmp",
+    args[j++] = "-Wl,--wrap=g_strcasecmp";
+    args[j++] = "-Wl,--wrap=g_strncasecmp";
 #endif /* _HF_ARCH_DARWIN */
 
     /* Pull modules defining the following symbols (if they exist) */
