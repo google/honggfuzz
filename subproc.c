274f2f149237e412fb39cea407f0b19555cb6a35
Robert Swiecki <robert@swiecki.net>
2020-03-26 11:57:04 +0100

    subproc: missing comma

--- subproc.c
+++ subproc.c
@@ -433,7 +433,7 @@ bool subproc_Run(run_t* run) {
 
     {
         static pthread_mutex_t local_mutex = PTHREAD_MUTEX_INITIALIZER;
-        MX_SCOPED_LOCK(&local_mutex)
+        MX_SCOPED_LOCK(&local_mutex);
         if (diffMillis >= ATOMIC_GET(run->global->timing.timeOfLongestUnitInMilliseconds)) {
             ATOMIC_SET(run->global->timing.timeOfLongestUnitInMilliseconds, diffMillis);
         }
