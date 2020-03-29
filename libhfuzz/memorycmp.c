adcbaf3d27472b6908d00ef937ff2ed382950fb1
Robert Swiecki <robert@swiecki.net>
2020-03-29 23:48:49 +0200

    hfuzz-cc: add a C++ wrapper

--- libhfuzz/memorycmp.c
+++ libhfuzz/memorycmp.c
@@ -542,3 +542,9 @@ HF_WEAK_WRAP(bool, g_str_has_suffix, const char* str, const char* suffix) {
     return (
         HF_strcmp(str + str_len - suffix_len, suffix, (uintptr_t)__builtin_return_address(0)) == 0);
 }
+
+/* C++ wrappers */
+int _ZNSt11char_traitsIcE7compareEPKcS2_m(const char* s1, const char* s2, size_t count) {
+    return HF_memcmp(
+        s1, s2, count, instrumentConstAvail(), (uintptr_t)__builtin_return_address(0));
+}
