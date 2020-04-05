e0d47db4d0d775a3dc42120351a17c646b198beb
Robert Swiecki <robert@swiecki.net>
2020-04-05 17:10:03 +0200

    libhfuzz/instrument: allow for more constdict values with cmp4 and with cmp8

--- libhfuzz/instrument.c
+++ libhfuzz/instrument.c
@@ -313,14 +313,14 @@ void __sanitizer_cov_trace_cmp2(uint16_t Arg1, uint16_t Arg2) {
 void __sanitizer_cov_trace_cmp4(uint32_t Arg1, uint32_t Arg2) {
     /* Add 4byte values to the const_dictionary if they exist within the binary */
     if (cmpFeedback && instrumentLimitEvery(4095)) {
-        if (Arg1 > 0xffff && Arg1 < 0xffff0000) {
+        if (Arg1 > 0xffff) {
             uint32_t bswp = __builtin_bswap32(Arg1);
             if (util_32bitValInBinary(Arg1) || util_32bitValInBinary(bswp)) {
                 instrumentAddConstMemInternal(&Arg1, sizeof(Arg1));
                 instrumentAddConstMemInternal(&bswp, sizeof(bswp));
             }
         }
-        if (Arg2 > 0xffff && Arg2 < 0xffff0000) {
+        if (Arg2 > 0xffff) {
             uint32_t bswp = __builtin_bswap32(Arg2);
             if (util_32bitValInBinary(Arg2) || util_32bitValInBinary(bswp)) {
                 instrumentAddConstMemInternal(&Arg2, sizeof(Arg2));
@@ -335,14 +335,14 @@ void __sanitizer_cov_trace_cmp4(uint32_t Arg1, uint32_t Arg2) {
 void __sanitizer_cov_trace_cmp8(uint64_t Arg1, uint64_t Arg2) {
     /* Add 8byte values to the const_dictionary if they exist within the binary */
     if (cmpFeedback && instrumentLimitEvery(4095)) {
-        if (Arg1 > 0xffff && Arg1 < 0xffffffffffff0000) {
+        if (Arg1 > 0xffffff) {
             uint64_t bswp = __builtin_bswap64(Arg1);
             if (util_64bitValInBinary(Arg1) || util_64bitValInBinary(bswp)) {
                 instrumentAddConstMemInternal(&Arg1, sizeof(Arg1));
                 instrumentAddConstMemInternal(&bswp, sizeof(bswp));
             }
         }
-        if (Arg2 > 0xffff && Arg2 < 0xffffffffffff0000) {
+        if (Arg2 > 0xffffff) {
             uint64_t bswp = __builtin_bswap64(Arg2);
             if (util_64bitValInBinary(Arg2) || util_64bitValInBinary(bswp)) {
                 instrumentAddConstMemInternal(&Arg2, sizeof(Arg2));
