76385987d72a7496ce65811f64d337cf636aead3
Robert Swiecki <robert@swiecki.net>
2020-04-27 01:02:11 +0200

    Move defer{} implementation to libhfuzz/util

--- libhfcommon/util.h
+++ libhfcommon/util.h
@@ -33,6 +33,71 @@
 #include <stdint.h>
 #include <time.h>
 
+/*
+ * Go-style defer scoped implementation
+ *
+ * If compiled with clang, use: -fblocks -lBlocksRuntime
+ *
+ * Example of use:
+ *
+ * {
+ *   int fd = open(fname, O_RDONLY);
+ *   if (fd == -1) {
+ *     error(....);
+ *     return;
+ *   }
+ *   defer { close(fd); };
+ *   ssize_t sz = read(fd, buf, sizeof(buf));
+ *   ...
+ *   ...
+ * }
+ *
+ */
+
+#define __STRMERGE(a, b) a##b
+#define _STRMERGE(a, b) __STRMERGE(a, b)
+#ifdef __clang__
+#if __has_extension(blocks)
+static void __attribute__((unused)) __clang_cleanup_func(void (^*dfunc)(void)) {
+    (*dfunc)();
+}
+
+#define defer                                        \
+    void (^_STRMERGE(__defer_f_, __COUNTER__))(void) \
+        __attribute__((cleanup(__clang_cleanup_func))) __attribute__((unused)) = ^
+
+#else /* __has_extension(blocks) */
+#define defer UNIMPLEMENTED - NO - SUPPORT - FOR - BLOCKS - IN - YOUR - CLANG - ENABLED
+#endif /*  __has_extension(blocks) */
+#else  /* !__clang__, e.g.: gcc */
+
+#define __block
+#define _DEFER(a, count)                                                                      \
+    auto void _STRMERGE(__defer_f_, count)(void* _defer_arg __attribute__((unused)));         \
+    int _STRMERGE(__defer_var_, count) __attribute__((cleanup(_STRMERGE(__defer_f_, count)))) \
+        __attribute__((unused));                                                              \
+    void _STRMERGE(__defer_f_, count)(void* _defer_arg __attribute__((unused)))
+#define defer _DEFER(a, __COUNTER__)
+#endif /* ifdef __clang__ */
+
+/* Block scoped mutexes */
+#define MX_SCOPED_LOCK(m) \
+    MX_LOCK(m);           \
+    defer {               \
+        MX_UNLOCK(m);     \
+    }
+
+#define MX_SCOPED_RWLOCK_READ(m) \
+    MX_RWLOCK_READ(m);           \
+    defer {                      \
+        MX_RWLOCK_UNLOCK(m);     \
+    }
+#define MX_SCOPED_RWLOCK_WRITE(m) \
+    MX_RWLOCK_WRITE(m);           \
+    defer {                       \
+        MX_RWLOCK_UNLOCK(m);      \
+    }
+
 #define HF_STR_LEN 8192
 #define HF_STR_LEN_MINUS_1 8191
 
@@ -89,7 +154,6 @@ __attribute__((always_inline)) static inline bool ATOMIC_BITMAP_SET(uint8_t* add
 #define HF_MAX(x, y) ((x > y) ? x : y)
 #define HF_MIN(x, y) ((x < y) ? x : y)
 #define HF_CAP(v, x, y) HF_MAX(x, HF_MIN(y, v))
-
 #define util_Log2(v) ((sizeof(unsigned int) * 8) - __builtin_clz((unsigned int)v) - 1)
 
 typedef enum {
