cf26a29323bd859ddc82568049c590d0fe421c24
Christian Heitman <cheitman@quarkslab.com>
2020-12-11 15:14:16 -0300

    Add --statsfile command-line option

--- honggfuzz.h
+++ honggfuzz.h
@@ -214,6 +214,8 @@ typedef struct {
         dynfile_t*  dynfileq2Current;
         TAILQ_HEAD(dyns_t, _dynfile_t) dynfileq;
         bool exportFeedback;
+        const char* statsFileName;
+        int         statsFileFd;
     } io;
     struct {
         int                argc;
